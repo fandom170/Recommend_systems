61547e2e1875a337d254b1871b3316b348db1518
Konstantin <fandom170@gmail.com>
2022-03-01 10:03:22 +0200

    hw 06 started

--- project/src/recommenders.py
+++ project/src/recommenders.py
@@ -9,17 +9,6 @@ from implicit.als import AlternatingLeastSquares
 from implicit.nearest_neighbours import ItemItemRecommender  # нужен для одного трюка
 from implicit.nearest_neighbours import bm25_weight, tfidf_weight
 
-import pandas as pd
-import numpy as np
-
-# Для работы с матрицами
-from scipy.sparse import csr_matrix
-
-# Матричная факторизация
-from implicit.als import AlternatingLeastSquares
-from implicit.nearest_neighbours import ItemItemRecommender  # нужен для одного трюка
-from implicit.nearest_neighbours import bm25_weight, tfidf_weight
-
 
 class MainRecommender:
     """Рекоммендации, которые можно получить из ALS
